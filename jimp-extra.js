bf4c3baf7503732699b32c23212b49e2ce0c1d43
Arkadiusz Nowakowski <arkadiusz.nowakowski@us.edu.pl>
2019-05-30 22:14:48 +0200

    feat: deafult spritesheet

--- jimp-extra.js
+++ jimp-extra.js
@@ -18,7 +18,7 @@ module.exports = {
                  * @url: https://www.w3.org/TR/compositing/#simplealphacompositing
                  */
                 let maskIdx = mask.getPixelIndex(x % mask.bitmap.width, y % mask.bitmap.height);
-                let as = mask.bitmap.data[maskIdx + 3] / 255;
+                let as = mask.bitmap.data[maskIdx + 3] / 255 / 3;
                 let ab = image.bitmap.data[idx + 3] / 255;
                 for (let i of [0, 1, 2]) {
                     let Cb = image.bitmap.data[idx + i] / 255;
